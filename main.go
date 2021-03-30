be3347c0900cfa6703efbb0ace1c8b3f2fcc1cf2
Max Baumann <info@fosefx.com>
2021-03-30 13:21:54 +0200

    add yml fixer

--- main.go
+++ main.go
@@ -3,10 +3,7 @@ package main
 import (
 	"encoding/xml"
 	"fmt"
-	"io/ioutil"
 	"os"
-	"path/filepath"
-	"strings"
 )
 
 type Resources struct {
@@ -22,103 +19,26 @@ type Public struct {
 }
 
 func main() {
-	args := os.Args[1:]
 
-	if len(args) < 2 {
-		fmt.Println("usage: public-fixer <path/to/base> [other-public.xml]")
+	cmd := ""
+	if len(os.Args) > 1 {
+		cmd = os.Args[1]
+	}
+
+	switch cmd {
+	case "fix":
+		Fix()
+	case "yml":
+		Yml()
+	default:
+		fmt.Println("public-fixer")
+		fmt.Println("available commands:")
+		fmt.Println("* fix - find and replace all placeholders in xml files")
+		fmt.Println("* yml - add DNC of splits to base apktool.yml")
+		fmt.Println("")
 		os.Exit(1)
 	}
 
-	base := args[0]
-	other := args[1:]
-
-	baseResources := xmlify(base + "/res/values/public.xml")
-
-	missingMap := make(map[string]int)    // map id to index in baseResources
-	changedMap := make(map[string]string) // map old name to new name
-
-	for i := 0; i < len(baseResources.Public); i++ {
-		public := baseResources.Public[i]
-		name := public.Name
-		id := public.Id
-		if strings.HasPrefix(name, "APKTOOL_DUMMY_") {
-			missingMap[id] = i
-		}
-	}
-
-	fmt.Println("Found", len(missingMap), "missing values")
-
-	for i := 0; i < len(other); i++ {
-		filename := other[i]
-		otherRes := xmlify(filename)
-
-		for j := 0; j < len(otherRes.Public); j++ {
-			public := otherRes.Public[j]
-			name := public.Name
-			id := public.Id
-			if baseIndex, ok := missingMap[id]; ok {
-				if strings.HasPrefix(name, "APKTOOL_DUMMY_") {
-					fmt.Println("Found id, but it's also a dummy:", id)
-				} else {
-					oldName := baseResources.Public[baseIndex].Name
-					baseResources.Public[baseIndex].Name = name
-					delete(missingMap, id)
-					changedMap[oldName] = name
-				}
-			}
-		}
-	}
-
-	if val := len(missingMap); val != 0 {
-		fmt.Println("Still", val, "missing names")
-	}
-
-	// replace in all xml files
-	err := filepath.Walk(base+"/res",
-		func(path string, info os.FileInfo, err error) error {
-			if err != nil {
-				return err
-			}
-			if !strings.HasSuffix(info.Name(), ".xml") {
-				return nil
-			}
-			file, err := os.OpenFile(path, os.O_RDWR, 0644)
-			handleErr(err)
-
-			bytes, err := ioutil.ReadAll(file)
-			handleErr(err)
-
-			str := string(bytes)
-
-			for old, newv := range changedMap {
-				str = strings.ReplaceAll(str, old + "\"", newv + "\"")
-				str = strings.ReplaceAll(str, old + "<", newv + "<")
-			}
-
-			err = file.Truncate(0)
-			handleErr(err)
-			_, err = file.WriteAt([]byte(str), 0)
-			handleErr(err)
-
-			err = file.Close()
-			handleErr(err)
-			return nil
-		})
-	handleErr(err)
-
-}
-
-func xmlify(path string) Resources {
-	file, err := os.Open(path)
-	handleErr(err)
-
-	defer file.Close()
-	byteValue, _ := ioutil.ReadAll(file)
-	var resources Resources
-	err = xml.Unmarshal(byteValue, &resources)
-	handleErr(err)
-
-	return resources
 }
 
 func handleErr(err error) {
---
+++ yml.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"sort"
+	"strings"
+)
+
+func Yml() {
+	args := os.Args[2:]
+
+	if len(args) < 2 {
+		fmt.Println("usage: public-fixer yml <path/to/base> [other-apktool.yml]")
+		os.Exit(1)
+	}
+
+	base := args[0]
+	other := args[1:]
+
+	all := make(map[string]bool)
+
+	basePath := base + "/apktool.yml"
+	getDNC(basePath, all)
+	for i := 0; i < len(other); i++ {
+		path := other[i]
+		getDNC(path, all)
+	}
+
+	pre, post := getPreAndPostDNC(basePath)
+	result := pre
+	for _, v := range mapToArray(all) {
+		result = result + "- " + v + "\n"
+	}
+	result += post
+	file, err := os.OpenFile(basePath, os.O_RDWR, 0644)
+	handleErr(err)
+	err = file.Truncate(0)
+	handleErr(err)
+	_, err = file.WriteAt([]byte(result), 0)
+	file.Close()
+}
+
+func readFile(path string) string {
+	file, err := os.Open(path)
+	handleErr(err)
+	defer file.Close()
+	bytes, err := ioutil.ReadAll(file)
+	handleErr(err)
+	content := string(bytes)
+	return content
+}
+
+func getDNC(path string, buffer map[string]bool) {
+	content := readFile(path)
+	scanner := bufio.NewScanner(strings.NewReader(content))
+
+	pre := true
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if pre {
+			// objective: find doNotCompress-line
+			if strings.HasSuffix(line, "doNotCompress:") {
+				pre = false
+			}
+		} else {
+			// objective: collect all items in array
+			if !strings.HasPrefix(line, "- ") {
+				break
+			} else {
+				buffer[line[2:]] = true
+			}
+		}
+	}
+}
+
+func getPreAndPostDNC(path string) (string, string) {
+	content := readFile(path)
+	scanner := bufio.NewScanner(strings.NewReader(content))
+	state := "pre"
+
+	preStr := ""
+	postStr := ""
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if state == "pre" {
+			if strings.HasSuffix(line, "doNotCompress:") {
+				state = "in"
+			} else {
+				preStr += line + "\n"
+			}
+		} else if state == "in" {
+			if strings.HasPrefix(line, "- ") {
+				continue
+			} else {
+				state = "post"
+			}
+		} else if state == "post" {
+			postStr += line + "\n"
+		}
+	}
+	return preStr, postStr
+}
+
+func mapToArray(m map[string]bool) []string {
+	var res []string
+	for k, _ := range m {
+		res = append(res, k)
+	}
+	sort.Strings(res)
+	return res
+}
